5d69c9f9213a407b2bcee27e16291df0ecd340dc
ahmeddoban <ahmedzoro323@gmail.com>
2022-09-04 23:39:47 +0200

    function date_hours(day

--- js/script.js
+++ js/script.js
@@ -17,6 +17,11 @@ function convertion(val) {
   return (val - 273).toFixed(0);
 }
 
+function date_hours(day) {
+  let hours = day.getHours();
+  hours % 12 == 0 ? hours : (hours -= 12);
+  return hours;
+}
 btn.addEventListener("click", function () {
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -33,7 +38,9 @@ btn.addEventListener("click", function () {
       var wndspd = data["wind"]["speed"];
       const icon = data["weather"]["0"]["icon"];
 
-      city.innerHTML = `<h1>${nameval}</h1> <span>${day.getHours()}:${day.getMinutes()} - ${day.getDate()}-${day.getMonth()}-${day.getFullYear()} </span>`;
+      city.innerHTML = `<h1>${nameval}</h1> <span>${date_hours(
+        day
+      )}:${day.getMinutes()} &nbsp; ${day.getDate()}-${day.getMonth()}-${day.getFullYear()} </span>`;
       temp.innerHTML = `<span>${convertion(tempature)}  &#176;</span>`;
       description.innerHTML = `<p>Sky Conditions </p> <span>${descrip}<span>`;
       wind.innerHTML = `Wind Speed <span>${wndspd} km/h<span>`;
